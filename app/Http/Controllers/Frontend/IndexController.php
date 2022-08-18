88bdc46b14961042beeb333c95804870c24b9738
MOHAMMED-BE <Bensghirsbih19@gmail.com>
2022-08-18 09:52:26 +0100

    add and refresh frontend template

---
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function Index(){
+        return view('frontend.index');
+    }
+
+    // public function signin(){
+    //     return view('auth.login');
+    // }
+}
---
+++ resources/views/frontend/main_master.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+    <meta name="robots" content="all">
+    <title>Flipmart premium HTML5 & CSS3 Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
+
+    <!-- Customizable CSS -->
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/blue.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.transitions.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/rateit.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap-select.min.css')}}">
+
+    <!-- Icons/Glyphs -->
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/font-awesome.css')}}">
+
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script> -->
+</head>
+
+<body class="cnt-home">
+    <!-- ============================================== HEADER ============================================== -->
+
+    @include('frontend.body.header')
+
+    <!-- ============================================== HEADER : END ============================================== -->
+
+    @yield('content')
+
+    <!-- /#top-banner-and-menu -->
+
+    <!-- ============================================================= FOOTER ============================================================= -->
+
+    @include('frontend.body.footer')
+
+    <!-- ============================================================= FOOTER : END============================================================= -->
+
+    <!-- For demo purposes – can be removed on production -->
+
+    <!-- For demo purposes – can be removed on production : End -->
+
+    <!-- JavaScripts placed at the end of the document so the pages load faster -->
+    <script src="{{asset('frontend/assets/js/jquery-1.11.1.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/bootstrap-hover-dropdown.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/echo.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/jquery.easing-1.3.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/bootstrap-slider.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/jquery.rateit.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('frontend/assets/js/lightbox.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/bootstrap-select.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/wow.min.js')}}"></script>
+    <script src="{{asset('frontend/assets/js/scripts.j')}}s"></script>
+</body>
+
+</html>
