1a437ebdb308912de6c8e539518e1579eb32db81
MOHAMMED-BE <Bensghirsbih19@gmail.com>
2022-08-21 18:27:29 +0100

    update sweetalert script

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -27,6 +27,34 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script type="text/javascript">
+    $(function() {
+      $(document).on('click', '#delete', function(e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+        Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = link
+            Swal.fire(
+              'Deleted!',
+              'Brand has been deleted.',
+              'success'
+            )
+          }
+        })
+      });
+    });
+  </script>
+
 </head>
 
 <body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
@@ -91,10 +119,10 @@
     }
     @endif
   </script>
-
+  <!-- 
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-  <!-- <script type="text/javascript">
+  <script type="text/javascript">
     $(function() {
       $(document).on('click', '#delete', function(e) {
         e.preventDefault();
